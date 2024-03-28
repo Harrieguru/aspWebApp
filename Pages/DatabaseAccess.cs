541796a1505f78e2b775771040097300bc2f7f0e
Harriet_atieno <66387509+Harrieguru@users.noreply.github.com>
2024-03-28 14:35:21 -0400

    DatabaseAccess.cs created

--- Pages/DatabaseAccess.cs
+++ Pages/DatabaseAccess.cs
@@ -2,5 +2,9 @@
 {
     public class DatabaseAccess
     {
+        public void OnGet()
+        {
+            Console.WriteLine("Database access has already been done on April 14, 2023 by Harriet Odhiambo");
+        }
     }
 }
